709f5541e8bdd1b73c45d18e60e9848034a5b3e4
Kevin Nielsen <knielsen0506@gmail.com>
2018-02-16 20:46:42 +0000

    added Header and Footer.ejs integrating Semantic UI , added new routes in app.js,

--- app.js
+++ app.js
@@ -2,12 +2,44 @@ var bodyParser    = require("body-parser"),
     mongoose       = require("mongoose"),
     express        = require("express"),
     app            = express();
-    
+ 
+ 
+//app config    
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Mongoose model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    title: "Test",
+    image: "https://images.unsplash.com/photo-1470328358326-dee4879da669?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=407e114dc47bc1df4ce0e4292f158576&auto=format&fit=crop&w=750&q=80",
+    body: "Hello"
+});
+
+//Show RESTfulnode Routes
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is listening");
