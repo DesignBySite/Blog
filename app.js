1c643e22a45bc3a34b7335632157351db589a219
Kevin Nielsen <knielsen0506@gmail.com>
2018-02-19 17:32:52 +0000

    add updated files

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var bodyParser    = require("body-parser"),
-    mongoose       = require("mongoose"),
-    express        = require("express"),
-    app            = express();
+var bodyParser     = require("body-parser"),
+methodOverride     = require("method-override"),
+expressSanitizer   = require("express-sanitizer"),
+mongoose           = require("mongoose"),
+express            = require("express"),
+app                = express();
  
  
 //app config    
@@ -9,6 +11,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 //Mongoose model config
 var blogSchema = new mongoose.Schema({
@@ -50,6 +54,7 @@ app.get("/blogs/new", function(req, res) {
 // Create Route
 
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, blog){
         if(err){
             res.render("new");
@@ -67,8 +72,42 @@ app.get("/blogs/:id", function(req, res) {
         } else {
             res.render("show", {blog: foundBlog});
         }
-    })
-})
+    });
+});
+
+//Edit Route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//Update Route
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs/" + req.params.id);
+       }
+   });
+});
+
+//Delete Route
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is listening");
